45f4092045eb9a3c7591dd8fd81d2f49d723cde0
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-20 17:04:45 +0200

    Backend: Adding an api to get blocked list of a user

---
+++ Backend/blocked_list.php
@@ -0,0 +1,15 @@
+<?php
+include("db_connection.php"); 
+
+
+if(isset($_GET['userId'])){
+    $user_id = $_GET['userId'];
+}
+
+$query = $mysqli->prepare("Select user_id2 as blocked_user FROM blocks where user_id1 = ? ");
+$query -> bind_param("i" , $user_id);
+$query -> execute();
+$array = $query-> get_result();
+$response = $array-> fetch_assoc();
+
+echo json_encode($response);
