d2c1bb74d04473cafa4bd9012df971cee43c2225
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-21 22:05:11 +0200

    Frontend: Finishing register linking

---
+++ Frontend/scripts/register.js
@@ -0,0 +1,30 @@
+window.onload = ()=>{
+    const usernameField = document.getElementById("username");
+    const emailField = document.getElementById("email");
+    const fullNameField = document.getElementById("fullName");
+    const passwordField = document.getElementById("password");
+    const button = document.getElementById("signupButton");
+
+    button.addEventListener("click" , async (e)=>{
+        e.preventDefault();
+
+        const username = usernameField.value;
+        const email  = emailField.value;
+        const password = passwordField.value;
+        const fullName = fullNameField.value; 
+
+        const data = new FormData();
+        data.append("username", username);
+        data.append("password", password);
+        data.append("email" , email);
+        data.append("name", fullName);
+        
+        axios.post("http://localhost/localInstagram/Backend/register.php" , data).then(response => {
+            const list  = response.data; 
+            console.log(list);
+            if(list['success'] && !list['exist']){
+                location.replace("login.html");
+            }
+        })
+    })
+};
