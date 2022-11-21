6c11575eea8f02f00f2e0288cec5be9716f278e7
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-21 22:21:59 +0200

    Backend: Adding conditions on retreive

--- Backend/retreive_posts.php
+++ Backend/retreive_posts.php
@@ -4,7 +4,10 @@ header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: * ");
 include("db_connection.php"); 
 
-$query = $mysqli-> prepare("SELECT * FROM posts");
+$id = $_GET['id'];
+
+$query = $mysqli-> prepare("SELECT * FROM posts where user_id != (Select user_id1 from blocks where user_id2 = ?");
+$query-> bind_param("i" , $id);
 
 $query->execute();
 $array = $query->get_result();
