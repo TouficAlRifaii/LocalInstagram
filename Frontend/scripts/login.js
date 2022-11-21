5938b46602b1a229cfa346dcc4553e7c93332122
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-21 21:47:50 +0200

    Frontend: Finishing first version of login

--- Frontend/scripts/login.js
+++ Frontend/scripts/login.js
@@ -2,17 +2,21 @@ window.onload = ()=> {
     const usernameField = document.getElementById("username");
     const loginButton = document.getElementById("login-button");
     const passwordField = document.getElementById("password");
+    const storage = window.localStorage;
     loginButton.addEventListener("click" , async(e)=>{
         e.preventDefault();
         const username = usernameField.value;
         const password = passwordField.value; 
-        console.log(password);
         const data = new FormData();
         data.append("username" , username);
         data.append("password" , password);
-        await axios.post("http://localhost/localInstagram/Backend/login.php",data).then(response => console.log(response.data));
-        const res = await axios.get("http://localhost/localInstagram/Backend/session.php");
-        console.log(res.data);
+        await axios.post("http://localhost/localInstagram/Backend/login.php",data).then(response =>{
+            const list = response.data; 
+            if(list['success']){
+                storage.setItem("session" , JSON.stringify(list));
+                location.replace("index.html");
+            }
+        });
     });
 
 
