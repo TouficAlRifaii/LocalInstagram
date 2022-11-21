1d3710df9ea5663e71b7a004387c5017e9e726d2
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-22 01:42:49 +0200

    Backend: Fixing create post api

--- Backend/create_post.php
+++ Backend/create_post.php
@@ -6,34 +6,24 @@ header("Access-Control-Allow-Headers: * ");
 include("db_connection.php");
 error_reporting(0);
  
-$msg = "";
- 
 // If upload button is clicked ...
-if (isset($_POST['description']) && isset($_POST['user_id']) && !empty($_FILES["uploadfile"]["name"])) {
+if (isset($_POST['description']) && isset($_POST['user_id']) && isset($_POST['file']) && $_POST['file'] != "") {
     $description = $_POST['description'];
     $user_id = $_POST['user_id'];
     $likes = 0; 
-    $filename = $_FILES["uploadfile"]["name"];
-    $tempname = $_FILES["uploadfile"]["tmp_name"];
-    $folder = "C:/Users/altou/OneDrive/Desktop/LocalInstagram/Frontend/" . $filename;
- 
-    $db = mysqli_connect("localhost", "root", "", "geeksforgeeks");
+    $image = $_POST['file'];
  
     // Get all the submitted data from the form
     $query = $mysqli-> prepare( "INSERT INTO posts (description , user_id , image_name , like_count) VALUES (? , ?, ? , ?)");
-    $query -> bind_param("sisi" , $description, $user_id , $filename , $likes);
+    $query -> bind_param("sisi" , $description, $user_id , $image , $likes);
+    $query-> execute(); 
+    $response = [] ;
+    $response["success"] = true; 
+    echo json_encode($response);
     // Execute query
-    $query-> execute; 
+    
  
     //  move the uploaded image into the folder: image
-    if (move_uploaded_file($tempname, $folder)) {
-        
-    } else {
-        $response = []; 
-        $response['success'] = false;
-        echo json_encode($response);
-        
-    }
 }
 else{
     $response = [] ;
