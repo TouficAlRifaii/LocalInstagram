ee244283dae559bc04aa51d6d095d59d79a9d1ad
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-22 03:46:54 +0200

    Backend: Fixing block user

--- Backend/block_user.php
+++ Backend/block_user.php
@@ -5,9 +5,9 @@ header("Access-Control-Allow-Headers: * ");
 
 include("db_connection.php");
 
-if(isset($_POST['user_id1']) && isset($_POST['user_id2'])){
+if(isset($_POST['user_id1']) && isset($_POST['username'])){
     $blocker = $_POST['user_id1'];
-    $blocked = $_POST['user_id2']; 
+    $blocked = $_POST['username']; 
 }
 else{
     $response = [];
@@ -15,10 +15,14 @@ else{
     echo json_encode($response); 
     return;
 }
-
+$getBlocked = $mysqli -> prepare("SELECT user_id from users where username = ?");
+$getBlocked  -> bind_param('s' , $blocked);
+$getBlocked -> execute(); 
+$array = $getBlocked -> get_result();
+$blocked_id = $array-> fetch_assoc();
 $query = $mysqli -> prepare("INSERT INTO blocks(user_id1 , user_id2) values(? , ?)");
-$query -> bind_param("ii" , $blocker , $blocked);
+$query -> bind_param("ii" , $blocker , $blocked_id['user_id']);
 $query-> execute();
 $response = []; 
-$response["success"] = "success";
+$response["success"] = true;
 echo json_encode($response);
