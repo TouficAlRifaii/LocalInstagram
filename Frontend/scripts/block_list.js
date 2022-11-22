d7f85308239f35529b31d42bd9f21ca02b0d6c70
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-11-22 03:57:57 +0200

    Frontend: Finishing block user and unblock

--- Frontend/scripts/block_list.js
+++ Frontend/scripts/block_list.js
@@ -3,6 +3,10 @@ window.onload = ()=>{
     const sessions = JSON.parse(storage.getItem("session"));
     const id = sessions['id'];
     const blocked_list = document.getElementById("blocked_list");
+    const blockedField = document.getElementById("block_username");
+    const blockButton = document.getElementById("block-button");
+    const unblockedField = document.getElementById("unblock_username");
+    const unblockButton = document.getElementById("unblock-button");
     axios.get("http://localhost/localInstagram/Backend/blocked_list.php?user_id="+id).then(response=>{
         const list = response.data;
         let htmlTable = "<table><tr><th>Username</th></tr></br>";
@@ -12,4 +16,30 @@ window.onload = ()=>{
         htmlTable += "</table>";
         blocked_list.innerHTML = htmlTable;
     });
+
+    blockButton.addEventListener("click" , (e)=>{
+        e.preventDefault();
+        const username = blockedField.value;
+        
+        const data = new FormData();
+        data.append("user_id1" , id);
+        data.append("username" , username);
+        axios.post("http://localhost/localInstagram/Backend/block_user.php", data).then(response =>{
+            location.reload();
+        })
+    })
+
+
+    unblockButton.addEventListener("click" , (e)=>{
+        e.preventDefault();
+        const username = unblockedField.value;
+        
+        const data = new FormData();
+        data.append("user_id1" , id);
+        data.append("username" , username);
+        axios.post("http://localhost/localInstagram/Backend/unblock_user.php", data).then(response =>{
+            location.reload();
+        })
+    })
+
 }
